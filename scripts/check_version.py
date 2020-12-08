594ba2fb0b1cbb13dbf9f0a6bac397ad75b9bc2a
tokebe <43009413+tokebe@users.noreply.github.com>
2020-12-08 13:52:57 -0500

    lower 3.x bound

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -2,7 +2,7 @@ import sys
 
 if sys.version_info[0] != 3:
     sys.exit(-1)
-elif sys.version_info[1] > 8:
+elif sys.version_info[1] > 8 or sys.version_info[1] < 6:
     sys.exit(-1)
 else:
     sys.exit(0)
