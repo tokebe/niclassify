cd43a37fcd350c84812ba4674c0a1604fd9fde1d
tokebe <43009413+tokebe@users.noreply.github.com>
2021-01-23 13:54:33 -0500

    rm test file

--- bad.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-:: call scripts\fchooser.bat
-set /p rloc=< "%~dp0\niclassify\core\utilities\config\rloc.txt"
-echo %rloc%
-echo "%rloc%\bin\Rscript.exe"
-::"%rloc%\bin\Rscript.exe" "scripts\check_r_reqs.R"
-endlocal
