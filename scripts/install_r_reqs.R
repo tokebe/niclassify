218f03fbbf24be766b63dc7234b20650d9142bfd
tokebe <43009413+tokebe@users.noreply.github.com>
2021-01-24 11:53:53 -0500

    skip already installed packages

--- scripts/install_r_reqs.R
+++ scripts/install_r_reqs.R
@@ -1,12 +1,12 @@
 #!/usr/bin/env Rscript
-# attempt to install required packages
-install.packages("ape", repos="https://cloud.r-project.org/")
-install.packages("adephylo", repos="https://cloud.r-project.org/")
-install.packages("phytools", repos="https://cloud.r-project.org/")
-install.packages("phangorn", repos="https://cloud.r-project.org/")
-install.packages("paran", repos="https://cloud.r-project.org/")
-install.packages("splits", repos="http://R-Forge.R-project.org")
-install.packages("seqinr", repos="https://cloud.r-project.org/")
-install.packages("stringr", repos="https://cloud.r-project.org/")
-install.packages("reshape2", repos="https://cloud.r-project.org/")
-install.packages("tidyverse", repos="https://cloud.r-project.org/")
+# attempt to install required packages, skipping those already installed
+if (!require(ape)) { install.packages("ape", repos="https://cloud.r-project.org/") }
+if (!require(adephylo)) { install.packages("adephylo", repos="https://cloud.r-project.org/") }
+if (!require(phytools)) { install.packages("phytools", repos="https://cloud.r-project.org/") }
+if (!require(phangorn)) { install.packages("phangorn", repos="https://cloud.r-project.org/") }
+if (!require(paran)) { install.packages("paran", repos="https://cloud.r-project.org/") }
+if (!require(splits)) { install.packages("splits", repos="http://R-Forge.R-project.org") }
+if (!require(seqinr)) { install.packages("seqinr", repos="https://cloud.r-project.org/") }
+if (!require(stringr)) { install.packages("stringr", repos="https://cloud.r-project.org/") }
+if (!require(reshape2)) { install.packages("reshape2", repos="https://cloud.r-project.org/") }
+if (!require(tidyverse)) { install.packages("tidyverse", repos="https://cloud.r-project.org/") }
