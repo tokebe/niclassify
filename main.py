18be31a6151eabdc8957da343a1b9aac07a800bc
tokebe <43009413+tokebe@users.noreply.github.com>
2021-07-19 13:07:17 -0400

    todos

--- main.py
+++ main.py
@@ -4,21 +4,12 @@ if __name__ == "__main__":
     gui.main()
 
 # NOW
-# TODO support mac/linux
-# TODO go through and find any uncommented/undercommented code and add comments
 # TODO go through all steps and see what is missing status updates
 
 
 # LATER
 # TODO implement commandline version, use for tests
-# TODO properly implement logging for most steps
 
 
 # LATERER
 # TODO try to move everything off of R to Python?
-# TODO add new tool for merging prepared data
-
-# FIXES & MINOR FEATURES
-# TODO go searching for problems, unexpected behaviors, etc
-# TODO minor issues:
-# main progressbar freezes on certain errors, such as not enough labels
