d8caef99b96b3a4157635d048b2d808228e4c770
tokebe <43009413+tokebe@users.noreply.github.com>
2024-06-04 09:29:24 -0700

    update test proceedures

--- test_procedure.sh
+++ test_procedure.sh
@@ -1,5 +1,7 @@
-nic sample get -g Massachusetts -t hemiptera -o testing/hemiptera_raw.tsv
-nic sample filter -i testing/hemiptera_raw.tsv -o testing/hemiptera_filter.tsv
-nic sample identify -i testing/hemiptera_filter.tsv -o testing/hemiptera_id.tsv
-nic sample lookup -i testing/hemiptera_id.tsv -o testing/hemiptera_lookup.tsv -g 29
-nic fasta align -i testing/hemiptera_lookup.tsv -o testing/hemiptera_align.fasta
+nic sample get -g Massachusetts -t hemiptera -o testing/hemiptera_raw.tsv --debug
+nic sample filter -i testing/hemiptera_raw.tsv -o testing/hemiptera_filter.tsv --debug
+nic sample identify -i testing/hemiptera_filter.tsv -o testing/hemiptera_id.tsv --debug
+nic sample lookup -i testing/hemiptera_id.tsv -o testing/hemiptera_lookup.tsv -g 29 --debug
+nic fasta align -i testing/hemiptera_lookup.tsv -o testing/hemiptera_align.fasta -a --debug
+nic fasta trim -i testing/hemiptera_align.fasta -o testing/hemiptera_trim.fasta --debug
+nic fasta delimit -i testing/hemiptera_lookup.tsv -f testing/hemiptera_trim.fasta -o testing/hemiptera_delim.tsv --debug
