c84e059f6f4b269951ffb4505cb00c7beede30d1
tokebe <43009413+tokebe@users.noreply.github.com>
2024-06-04 09:29:10 -0700

    write/align fixes

--- niclassify/core/write/write.py
+++ niclassify/core/write/write.py
@@ -10,6 +10,7 @@ from threading import Lock
 # TODO: if a split file ends up with only 1 sequence, report that it can't be used
 # given the split level and then figure out how to proceed without it
 
+
 def write(
     data: DataFrame,
     splits: List[str] | None,
@@ -21,67 +22,42 @@ def write(
 ) -> List[Path]:
     row_count = data.shape[0].compute()
 
-    if output_all:
-        if splits is None:
-            files = {
-                "file": (
-                    Lock(),
-                    open(
-                        output_file.parent
-                        / f"{output_file.stem}_nosplit_unaligned{output_file.suffix}",
-                        "w",
-                        encoding="utf8",
-                    ),
+    if splits is None:
+        splits = ["nosplit"]
+    files = {
+        split: (
+            Lock(),
+            (
+                open(
+                    output_file.parent
+                    / f"{output_file.stem}_{split}_unaligned{output_file.suffix}",
+                    "w",
+                    encoding="utf8",
                 )
-            }
-        else:
-            files = {
-                split: (
-                    Lock(),
-                    open(
-                        output_file.parent
-                        / f"{output_file.stem}_{split}_unaligned{output_file.suffix}",
-                        "w",
-                        encoding="utf8",
-                    ),
+                if output_all
+                else NamedTemporaryFile(
+                    suffix=f"_{split}_unaligned{output_file.suffix}",
+                    mode="w",
+                    encoding="utf8",
+                    delete=False,
                 )
-                for split in splits
-            }
-    else:
-        if splits is None:
-            files = {
-                "file": (
-                    Lock(),
-                    NamedTemporaryFile(
-                        suffix=f"_nosplit_unaligned{output_file.suffix}",
-                        mode="w",
-                        encoding="utf8",
-                        delete=False,
-                    ),
-                )
-            }
-        else:
-            files = {
-                split: (
-                    Lock(),
-                    NamedTemporaryFile(
-                        suffix=f"_{split}_unaligned{output_file.suffix}",
-                        mode="w",
-                        encoding="utf8",
-                        delete=False,
-                    ),
-                )
-                for split in splits
-            }
+            ),
+        )
+        for split in splits
+    }
 
+    entries_written: dict[str, int] = {}
+    file_map: dict[str, str] = {}
     try:
         with handler.progress(percent=True) as status:
             task = status.add_task(description="Writing to FASTA", total=row_count)
 
             def write_fasta(row):
                 if splits is None:
-                    lock, file = files["file"]
+                    split_name = "nosplit"
+                    lock, file = files["nosplit"]
                 else:
+                    split_name = row[f"{split_level.value}_name"]
                     lock, file = files[row[f"{split_level.value}_name"]]
                 with lock:
                     if splits is None:
@@ -90,10 +66,14 @@ def write(
                         label = f">{row[f'{split_level.value}_name']}_{row['UID']}\n"
                     file.write(label)
                     file.write(f"{row['nucleotides']}\n")
+                    if split_name not in entries_written:
+                        entries_written[split_name] = 0
+                        file_map[split_name] = file.name
+                    entries_written[split_name] += 1
                 status.advance(task)
 
-            def compute_part(df):
-                df.apply(write_fasta, axis="columns")
+            def compute_part(df: DataFrame):
+                df.apply(write_fasta, axis=1)
                 return df
 
             data.map_partitions(
@@ -103,4 +83,27 @@ def write(
         for _, file in files.values():
             file.close()
 
-    return [Path(file.name) for _, file in files.values()]
+    ask = False
+    for split_name, n_written in entries_written.items():
+        if n_written <= 1:
+            if split_name == "nosplit":
+                handler.error(
+                    "There are insufficient samples to perform an alignment. Alignment aborted.",
+                    abort=True,
+                )
+            ask = True
+            handler.warning(
+                f"Split [bold]{split_name}[/] has insufficient samples to perform an alignment and will be skipped."
+            )
+    if ask:
+        handler.confirm("Continue with skipped files? (Abort otherwise)", abort=True)
+
+    keep_files = [
+        file_map[split_name]
+        for split_name, n_written in entries_written.items()
+        if n_written > 1
+    ]
+
+    return [
+        Path(file.name) for _, file in files.values() if file.name in keep_files
+    ]
