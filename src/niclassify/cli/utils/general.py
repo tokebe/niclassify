aeaa7a4ad2b284085097d7d03bbf889b4d4f5e26
tokebe <43009413+tokebe@users.noreply.github.com>
2025-07-31 16:38:45 -0400

    remove override decorator
    not available in python 3.11

--- src/niclassify/cli/utils/general.py
+++ src/niclassify/cli/utils/general.py
@@ -1,10 +1,8 @@
 # Order commands in order defined
-from typing import override
 
 from typer.core import TyperGroup
 
 
 class NaturalOrderGroup(TyperGroup):
-    @override
     def list_commands(self, ctx):
         return list(self.commands)
